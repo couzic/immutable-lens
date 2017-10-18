9bde746ac06abb252dd4dc53bc81264d8e1615a3
Mikael Couzic <mikaelcouzic@gmail.com>
2017-10-18 12:24:43 +0200

    Solve lib loading problem for testCompilationErrors()

--- testCompilationErrors.js
+++ testCompilationErrors.js
@@ -10,13 +10,17 @@ const path = require('path')
 
 const tsOptions = {noImplicitAny: false, noEmit: true, strict: true, target: 'es5'}
 
+const lib = require('./config/base/tsconfig.json').compilerOptions.lib
+
 glob(path.resolve(__dirname, 'src/*.shouldNotCompile.ts')).then(forEach(testFileForCompilationErrors))
+
+const libPaths = lib.map(lib => 'node_modules/typescript/lib/lib.' + lib + '.d.ts')
 
 function testFileForCompilationErrors(path) {
    const fileName = last(path.split('/'))
    console.log(chalk.blue('Testing compilation errors in "' + fileName + '" :'))
    const expectedErrorCount = (fs.readFileSync(path, 'utf8').match(/@shouldNotCompile/g) || []).length
-   const program = ts.createProgram([path], tsOptions)
+   const program = ts.createProgram([...libPaths, path], tsOptions)
    const diagnostics = ts.getPreEmitDiagnostics(program)
    if (diagnostics.length === expectedErrorCount) {
       console.log(chalk.green('All the expected compilation errors were found'))
